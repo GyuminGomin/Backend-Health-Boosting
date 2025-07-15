d8f8bf3fd9cdbc81cc489b73d7e583e46e4cc563
GyuminGomin <ool9898@naver.com>
2025-07-15 23:58:31 +0900

    .env 설정중

--- src/main/java/com/test/healthboosting/common/config/JwtTokenProvider.java
+++ src/main/java/com/test/healthboosting/common/config/JwtTokenProvider.java
@@ -4,14 +4,18 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.time.Instant;
 import java.util.Date;
 import java.util.List;
@@ -20,26 +24,32 @@ import java.util.List;
 public class JwtTokenProvider {
 
     @Value("${jwt.secret-key}")
-    private String SECRET_KEY;
+    private String secretKeyString;
+    private SecretKey secretKey;
+    private JwtParser jwtParser;
     private static final long TOKEN_VALIDITY = 3600000L; // 1시간
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostConstruct
+    public void init() {
+        secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKeyString));
+        jwtParser = Jwts.parser().verifyWith(secretKey).build();
+    }
 
     public String createToken(String userId, List<String> roles) {
         Instant now = Instant.now(); // 현재 시간을 Instant로 가져옴
         return Jwts.builder()
-                .claim("sub", userId) // setSubject 대신 sub 클레임 사용
+                .subject(userId)
+                .expiration(Date.from(now.plusMillis(TOKEN_VALIDITY)))
+                .issuedAt(Date.from(now))
                 .claim("roles", roles) // 사용자 정의 클레임 (권한 부여)
-                .claim("iat", Date.from(now)) // setIssuedAt 대신 iat 클레임 사용
-                .claim("exp", Date.from(now.plusMillis(TOKEN_VALIDITY))) // setExpiration 대신 exp 클레임 사용
-                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes())) // 서명 키 설정
+                .signWith(secretKey) // 서명 키 설정
                 .compact();
     }
 
     public Authentication getAuthentication(String token) {
         try {
-            Claims claims = Jwts.parser()
-                    .verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
-                    .build()
+            Claims claims = jwtParser
                     .parseSignedClaims(token)
                     .getPayload();
 
